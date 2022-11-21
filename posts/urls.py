f7a9349665d9f09037fe1d8f77f838e1ee1c06de
Julian Lopez <a20490712@itmexicali.edu.mx>
2022-11-21 14:41:16 -0800

    Added new post, list and detail views

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
     path('list/', PostListView.as_view(), name='list'),
+    path('details/<int:pk>/', PostDetailView.as_view(), name='detail'),
+    path('new/', PostCreateView.as_view(), name='new'),
 ]
