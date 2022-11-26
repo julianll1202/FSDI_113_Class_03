77514610bb204237ec980a6442022cf765070b87
Julian Lopez <a20490712@itmexicali.edu.mx>
2022-11-26 12:07:11 -0800

    Added password change and reset options, as well as a status option for posts

--- posts/views.py
+++ posts/views.py
@@ -8,18 +8,21 @@ from .models import Post
 from django.urls import reverse_lazy
 
 
+# List of posts view
 class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
 
+# Posts detail view
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
     model = Post
 
+# New post view
 class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "posts/new.html"
     model = Post
-    fields = ['title', 'subtitle', 'body']
+    fields = ['title', 'subtitle', 'body', 'status']
 
     # it is called whenever we submit the form
     def form_valid(self, form):
@@ -28,15 +31,17 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         # calls the original form to update it
         return super().form_valid(form)
 
+# Update post view
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = "posts/edit.html"
     model = Post
-    fields = ['title', 'subtitle', 'author', 'body']
+    fields = ['title', 'subtitle', 'author', 'body', 'status']
 
     def test_func(self):
         post_obj = self.get_object()
         return self.request.user == post_obj.author
 
+# Delete post view
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     template_name = "posts/delete.html"
     model = Post
